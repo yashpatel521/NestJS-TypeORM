115b73149f4f5e183431af0cd0f5c30046b6af06
Yash Patel <yash1451999@gmail.com>
2022-11-15 13:40:52 +0530

    env / gitignore added

---
+++ src/ApiResponsExample/role.ts
@@ -0,0 +1,21 @@
+export const roleCreate = {
+  statusCode: 200,
+  success: true,
+  data: {
+    id: 1,
+    name: "admin",
+    createdAt: "2022-11-14T12:14:00.295Z",
+  },
+};
+
+export const roleList = {
+  statusCode: 200,
+  succuss: true,
+  data: [
+    {
+      id: 1,
+      name: "admin",
+      createdAt: "2022-11-14T12:14:00.295Z",
+    },
+  ],
+};
--- src/ApiResponsExample/user.ts
+++ src/ApiResponsExample/user.ts
@@ -1,16 +1,18 @@
 export const userCreate = {
+  statusCode: 200,
   success: true,
   data: {
     id: 18,
-    email: 'yasoddsdh@yash.com',
-    name: 'yash',
-    password: '$2b$12$iCsMSjt27EKnQlFoDVunXe5BN',
+    email: "yasoddsdh@yash.com",
+    name: "yash",
+    password: "$2b$12$iCsMSjt27EKnQlFoDVunXe5BN",
+    createdAt: "2022-11-14T12:14:00.295Z",
+    role: {
+      id: 1,
+      name: "admin",
+      createdAt: "2022-11-14T12:14:00.295Z",
+    },
   },
-};
-
-export const userCreateError = {
-  success: false,
-  message: 'User creation failed',
 };
 
 export const userList = {
@@ -19,26 +21,45 @@ export const userList = {
   data: [
     {
       id: 25,
-      name: 'yash',
-      email: 'yadsdss@yash.com',
-      password: '$2b$12$FQ9GU4y2HTJ/TU8hmJ3Kb.ebRbYazJH67FrfDYwbbKpqn2V8Ye2EC',
+      name: "yash",
+      email: "yadsdss@yash.com",
+      password: "$2b$12$FQ9GU4y2HTJ/TU8hmJ3Kb.ebRbYazJH67FrfDYwbbKpqn2V8Ye2EC",
+      createdAt: "2022-11-14T12:14:00.295Z",
+      role: {
+        id: 1,
+        name: "admin",
+        createdAt: "2022-11-14T12:14:00.295Z",
+      },
     },
     {
       id: 24,
-      name: 'yash',
-      email: 'yash@yash.com',
-      password: '$2b$12$iCsMSjt27EKnQlFoDVunXe5BNs9cSKfv9cEfdj6tkiMGIQbrbEx/q',
+      name: "yash",
+      email: "yash@yash.com",
+      password: "$2b$12$iCsMSjt27EKnQlFoDVunXe5BNs9cSKfv9cEfdj6tkiMGIQbrbEx/q",
+      createdAt: "2022-11-14T12:14:00.295Z",
+      role: {
+        id: 1,
+        name: "admin",
+        createdAt: "2022-11-14T12:14:00.295Z",
+      },
     },
   ],
 };
+
 export const login = {
   statusCode: 200,
   succuss: true,
   data: {
-    id: 24,
-    name: 'yash',
-    email: 'yash@yash.com',
+    id: 6,
+    email: "yash@yash.com",
+    name: "Yash Patel",
+    createdAt: "2022-11-14T12:14:33.035Z",
+    role: {
+      id: 1,
+      name: "admin",
+      createdAt: "2022-11-14T12:14:00.295Z",
+    },
     access_token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjQsImlhdCI6MTY2ODIyNTY2NiwiZXhwIjoxNjY5MDg5NjY2fQ.0NzAYtwxIw75rbqGxdL1R1pq9OwxapRMGPn3wR8JP7c',
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiaWF0IjoxNjY4NDg3NDc0LCJleHAiOjE2NjkzNTE0NzR9.f5iuJUQiEN-axWPtu-_7VxJO1N61Cq5zwsaDVa5DJ9w",
   },
 };
