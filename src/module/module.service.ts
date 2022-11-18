2f3fd36a0951c6dd5035bd9fd1b2ce78aa91c9f2
Yash Patel <yash1451999@gmail.com>
2022-11-18 15:12:58 +0530

    pretty code

--- src/module/module.service.ts
+++ src/module/module.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { DeepPartial, Repository } from "typeorm";
 import { Modules } from "./entities/module.entity";
-import { Role } from "../role/entities/role.entity";
 
 @Injectable()
 export class ModuleService {
